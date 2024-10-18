69348838056163e013cdc95162794a2c21305e71
raghu23-dev <raghu2308.dev@gmail.com>
2024-10-18 11:12:58 +0530

    Set up basic Express server with a GET route for root endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("Server is running");
+import express from "express";
+const app = express();
+const PORT = 7000;
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+app.listen(/*PORT*/ PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});

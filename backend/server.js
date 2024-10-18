725e1c51911a070dd32db4eb3fd2b89f96da6e38
raghu23-dev <raghu2308.dev@gmail.com>
2024-10-18 12:52:12 +0530

    feature: Set up MongoDB connection
    - Implemented MongoDB connection using mongoose with error handling.
    - Configured server to connect to MongoDB upon startup.
    - Added dotenv for environment variable management.
    - Adjusted server to use PORT from environment variables or default to 3000.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+dotenv.config();
 const app = express();
-const PORT = 7000;
+const PORT = process.env.PORT || 3000;
 app.use("/api/auth", authRoutes);
-
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
+  connectMongoDB();
 });

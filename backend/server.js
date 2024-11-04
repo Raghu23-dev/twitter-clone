7e8279aded8c121b223a27e5f1e15a3e6c11fa2f
raghu23-dev <raghu2308.dev@gmail.com>
2024-11-04 21:23:27 +0530

    feature: Added user profile retrieval endpoint with protected route
    - Implemented getUserProfile controller to fetch user details excluding the password.
    - Added route for fetching user profile by username with authentication middleware.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
-import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectMongoDB from "./db/connectMongoDB.js";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +14,7 @@ app.use(express.json()); //to parse req.body
 app.use(express.urlencoded({ extended: true })); //to parse form data(urlencoded)
 app.use(cookieParser()); //to parse cookies
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
   connectMongoDB();
